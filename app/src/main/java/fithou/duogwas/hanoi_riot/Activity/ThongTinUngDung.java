99e5baa73fa942f6e5d654d518339acbe5ac92ef
duogwas <duong2001n@gmail.com>
2021-12-07 21:18:31 +0700

    done layout ThongTinUngDung

--- app/src/main/java/fithou/duogwas/hanoi_riot/Activity/ThongTinUngDung.java
+++ app/src/main/java/fithou/duogwas/hanoi_riot/Activity/ThongTinUngDung.java
@@ -2,15 +2,28 @@ package fithou.duogwas.hanoi_riot.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 import fithou.duogwas.hanoi_riot.R;
 
 public class ThongTinUngDung extends AppCompatActivity {
+    ImageButton img_BtnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_thong_tin_ung_dung);
+        img_BtnHome = findViewById(R.id.img_btnHome);
+        img_BtnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ThongTinUngDung.this, MainActivity.class));
+            }
+        });
     }
 }
