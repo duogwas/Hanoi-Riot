c00a355a3b3f819daeae3975503649fa073e322a
duogwas <duong2001n@gmail.com>
2021-12-07 21:12:54 +0700

    done layout HoTro

--- app/src/main/java/fithou/duogwas/hanoi_riot/Activity/HoTro.java
+++ app/src/main/java/fithou/duogwas/hanoi_riot/Activity/HoTro.java
@@ -2,15 +2,28 @@ package fithou.duogwas.hanoi_riot.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 import fithou.duogwas.hanoi_riot.R;
 
 public class HoTro extends AppCompatActivity {
+    ImageButton img_BtnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_ho_tro);
+        img_BtnHome = findViewById(R.id.img_btnHome);
+        img_BtnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HoTro.this, MainActivity.class));
+            }
+        });
     }
 }
